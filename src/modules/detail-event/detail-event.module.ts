6cfe70f30fb92f30a2a2e315573c8e90714d470e
Lê Thanh Toàn <toan20040331@gmail.com>
2024-10-28 08:55:13 +0700

    add repository event, detailevent in module

--- src/modules/detail-event/detail-event.module.ts
+++ src/modules/detail-event/detail-event.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DetailEventController } from './detail-event.controller';
 import { DetailEventService } from './detail-event.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import DetailEvent from 'src/entities/detail-event.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DetailEvent])],
   controllers: [DetailEventController],
-  providers: [DetailEventService]
+  providers: [DetailEventService],
 })
 export class DetailEventModule {}
--- src/modules/event/event.module.ts
+++ src/modules/event/event.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EventController } from './event.controller';
 import { EventService } from './event.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Events from 'src/entities/event.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Events])],
   controllers: [EventController],
-  providers: [EventService]
+  providers: [EventService],
 })
 export class EventModule {}
